f790ef7114fb509179fe5a73406b8bff270ce796
VoduDay <andriy.maniak@gmail.com>
2020-10-19 17:13:13 +0300

    Updates

---
+++ precache-manifest.31e9c091f2e7591990b5934c070aaa17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0efb08906043d16d79fa1e879b903c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d822568a2cfe7f336a21",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "c13110579975a1f28eff",
+    "url": "./static/css/main.8a2ebc01.chunk.css"
+  },
+  {
+    "revision": "d822568a2cfe7f336a21",
+    "url": "./static/js/2.cd70d490.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cd70d490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13110579975a1f28eff",
+    "url": "./static/js/main.fa16cc4f.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.31e9c091f2e7591990b5934c070aaa17.js"
 );
 
 self.addEventListener('message', (event) => {
